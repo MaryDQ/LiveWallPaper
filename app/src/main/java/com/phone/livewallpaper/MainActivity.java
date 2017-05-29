61a5bf854a4a97979ba55680763ba2dbdd373b39
MarkChaifer <964573692@qq.com>
2017-05-29 12:02:48 +0800

    调用VideoLiveWallpaper

--- app/src/main/java/com/phone/livewallpaper/MainActivity.java
+++ app/src/main/java/com/phone/livewallpaper/MainActivity.java
@@ -1,13 +1,41 @@
 package com.phone.livewallpaper;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.WallpaperManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox mCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCheckBox= (CheckBox) findViewById(R.id.checkBox1);
+
+        mCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    VideoLiveWallpaper.voiceSilence(getApplicationContext());
+                }else{
+                    VideoLiveWallpaper.voiceNormal(getApplicationContext());
+                }
+            }
+        });
+    }
+
+    public static void setToWallPaper(Context context){
+        final Intent intent=new Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER);
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+                new ComponentName(context,VideoLiveWallpaper.class));
+        context.startActivity(intent);
     }
 }
